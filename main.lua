2c8735d3a12ac3e183086dbc2e0d92daafed6d8b
josevictormoreno <josevictor.admoreno@gmail.com>
2022-06-15 20:36:14 -0300

    pong 3 and start of pong 4

--- main.lua
+++ main.lua
@@ -16,8 +16,16 @@ function love.load()
     -- used to prevent blurring of text
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    textFont = love.graphics.newFont('font.ttf', 8)
+    textFont = love.graphics.newFont('font.ttf',8)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+    
     love.graphics.setFont(textFont)
+
+    playerOne = 30
+    playerTwo = VIRTUAL_HEIGHT - 50
+
+    playerOneScore = 0
+    playerTwoScore = 0
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -25,6 +33,23 @@ function love.load()
         vsync = true
     })
 end
+
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        playerOne = playerOne - 200 * dt
+
+    elseif love.keyboard.isDown('s') then
+        playerOne = playerOne + 200 * dt
+    end
+
+    if love.keyboard.isDown('up') then
+        playerTwo = playerTwo - 200 * dt
+
+    elseif love.keyboard.isDown('down') then
+        playerTwo = playerTwo + 200 * dt
+    end 
+
+end 
 
 function love.keypressed(key)
     if key == 'escape' then
@@ -39,12 +64,17 @@ function love.draw()
     
     -- black to clean the screen
     love.graphics.clear( 0, 0, 0, 255) 
-    
+
+    love.graphics.setFont(textFont)
     love.graphics.printf('Hello Pong:', 0,  20, VIRTUAL_WIDTH, 'center')
 
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(playerOneScore), VIRTUAL_WIDTH/2-50, VIRTUAL_HEIGHT/3)
+    love.graphics.print(tostring(playerOneScore), VIRTUAL_WIDTH/2+30, VIRTUAL_HEIGHT/3)
+
     -- Draw the two rectangles for player one and two, and also the ball;
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', 10, playerOne, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, playerTwo, 5, 20)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH /2 -2, VIRTUAL_HEIGHT/2 -2,4,4)
 
     --end push
