55066eba6fb3b54656e28f83c3235ce7eb462d1b
josevictormoreno <josevictor.admoreno@gmail.com>
2022-06-14 22:37:19 -0300

    commit inicial

---
+++ main.lua
@@ -0,0 +1,11 @@
+-- Game Pong
+-- author:  José Victor Andrei Dalto Moreno
+-- @josevictormoreno -> https://github.com/josevictormoreno
+
+-- push is a library that will allow us to draw our game at a virtual
+-- resolution, instead of however large our window is; used to provide
+-- a more retro aesthetic
+-- https://github.com/Ulydev/push
+
+push = require 'push''
+
